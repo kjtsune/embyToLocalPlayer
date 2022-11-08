7ae9e9d6dd7942a7893c070e01bc2154e3d36406
kjtsune <kjtsune@outlook.com>
2022-11-08 11:00:47 +0800

    适配 qb 4.4+

--- qbittorrent_webui_open_file/qbittorrent_webui_open_file.js
+++ qbittorrent_webui_open_file/qbittorrent_webui_open_file.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         qbittorrent_webui_open_file
 // @namespace    https://github.com/kjtsune/embyToLocalPlayer/tree/main/qbittorrent_webui_open_file
-// @version      0.1
+// @version      0.2
 // @description  需要python。在 qBittorrent WebUI 里打开文件夹或者播放文件。
 // @description:zh-CN 需要python。在 qBittorrent WebUI 里打开文件夹或者播放文件。
 // @description:en  Require python. open folder or play media file from qb webui.
@@ -18,8 +18,8 @@ function checkAndAddElement() {
     let openButton = document.querySelector('a#openButton');
     if (infoTable && !openButton) {
         let savePath = infoTable.querySelector('#save_path');
-        savePath.insertAdjacentHTML('beforeBegin', 
-        `<a id="openButton">打开</a> <span> </span> <a id="playButton">播放</a>`);
+        savePath.insertAdjacentHTML('beforeBegin',
+            `<a id="openButton">打开</a> <span> </span> <a id="playButton">播放</a>`);
         let openButton = infoTable.querySelector('a#openButton');
         let playButton = infoTable.querySelector('a#playButton');
         openButton.addEventListener("click", openFolderFn, false);
@@ -37,17 +37,23 @@ function playMediaFile() {
 
 async function sendTorrentInfoAndOperate(operate) {
     let data = await getTorrentInfo();
-    let result = {info: data[0], file: data[1]}
+    let result = { info: data[0], file: data[1] }
     sendDataToLocalServer(result, operate)
 }
 
 async function getTorrentInfo() {
-    let torrentHash = document.querySelector('td#torrent_hash');
-    let hashText = torrentHash.textContent;
-    let info =  await fetch(`${window.location.href}api/v2/torrents/info?hashes=${hashText}`)
-    .then(r => r.json());
-    let fileList =  await fetch(`${window.location.href}api/v2/torrents/files?hash=${hashText}`)
-    .then(r => r.json());
+    let torrentHashList = document.querySelectorAll('td[id^=torrent_hash]');
+    for (let index = 0; index < torrentHashList.length; index++) {
+        const torrentHash = torrentHashList[index];
+        var hashText = torrentHash.textContent;
+        console.log('index', index, 'hash', hashText)
+        if (hashText.length > 15) break;
+    }
+
+    let info = await fetch(`${window.location.href}api/v2/torrents/info?hashes=${hashText}`)
+        .then(r => r.json());
+    let fileList = await fetch(`${window.location.href}api/v2/torrents/files?hash=${hashText}`)
+        .then(r => r.json());
     let result = [info, fileList]
     return result
 }
