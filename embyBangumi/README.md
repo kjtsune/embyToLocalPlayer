08fe00a2fa2bb3f9c542caf0d17a95c36799d5cd
kjtsune <110025665+kjtsune@users.noreply.github.com>
2023-08-23 11:00:09 +0800

    init embyBangumi

---
+++ embyBangumi/README.md
@@ -0,0 +1,34 @@
+## embyBangumi
+
+使用 Bangumi 的首季评分来填充 Emby 内的烂番茄评分（影评人评分）
+
+### 原理
+
+利用 Emby 从 TMDB 里刮削出的 `原产地名称` 和 `上映时间` ，通过 `api.bgm.tv` 检索评分。
+
+### 使用说明
+
+**ini 文件配置，先保持 `dry_run = yes` 来测试效果。**  
+**没有还原功能，使用前做好备份**
+
+1. 下载 `embyBangumi.zip`
+   并解压到任意文件夹。[发布页](https://github.com/kjtsune/embyToLocalPlayer/releases/tag/embyBangumi)
+2. 根据注释在`_config.ini` 配置文件填写 `host` `api_key` `user_id`这三项。
+3. 在解压文件夹里打开终端。
+4. 安装依赖：`python -m pip install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple requests`
+5. 运行命令：`python embyBangumi.py`
+6. 媒体库节目列表 > 右上角：••• > 勾选显示：影评人评分
+
+### 其他
+
+* 评分和上映时间都是以首季为准。
+* 电影类的上映时间不同地区区别较大，有的会搜索失败。  
+  搜索无结果后会把上映时间范围延长200天，再次搜索。准确率会降低。
+* 日志出现 `trust < 0.5` ,是因为搜索出来的结果不正确。会跳过不更改。  
+  错误的结果会缓存7天，7天后重新运行会再次尝试搜索。
+* 剧集类的上映时间一般没问题，所以不会再延长搜索的时间范围二次搜索。  
+  日志出现 `not result` 有可能是 NSFW 条目限制。  
+  或者 Emby 里的上映日期和 Bangumi 的相差超过两天。  
+  `not result` 的状态也会保持7天，7天后运行才会重试。
+* 重试大概率还是一样结果。（目前不考虑申请密钥）
+* 正确的结果，会根据发布时间长短缓存。90天内发布缓存3天。1年内的缓存30天。超过1年的缓存120天。
---
+++ embyBangumi/embyBangumi.py
@@ -0,0 +1,399 @@
+import datetime
+import difflib
+import json
+import os.path
+import re
+from configparser import ConfigParser
+
+import requests
+
+
+class Configs:
+    def __init__(self):
+        self.cwd = os.path.dirname(__file__)
+        self.path = [os.path.join(self.cwd, 'embyBangumi' + ext) for ext in (
+            f'-dev.ini', '-test.ini', '_config.ini')]
+        self.path = [i for i in self.path if os.path.exists(i)][0]
+        print(self.path)
+        self.raw = ConfigParser()
+        self.raw.read(self.path, encoding='utf-8-sig')
+
+    def get(self, key, fallback=''):
+        return self.raw.get('emby', key, fallback=fallback).strip()
+
+    def get_int(self, key, fallback=0):
+        return self.raw.getint('emby', key, fallback=fallback)
+
+
+class EmbyApi:
+    def __init__(self, host, api_key, user_id, http_proxy=None):
+        self.host = host.rstrip('/')
+        self.api_key = api_key
+        self.user_id = user_id
+        self.req = requests.Session()
+        self.req.headers.update({'Accept': 'application/json',
+                                 'Connection': 'keep-alive', })
+        self.req.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                                               '(KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+                                 'Referer': f'{self.host}/web/index.html'})
+        if http_proxy:
+            self.req.proxies = {'http': http_proxy, 'https': http_proxy}
+
+    def get(self, path, params=None):
+        params = params or {'X-Emby-Token': self.api_key}
+        params.update({'X-Emby-Token': self.api_key})
+        url = rf'{self.host}/emby/{path}'
+        return self.req.get(
+            url,
+            params=params,
+        )
+
+    def post(self, path, _json, params=None):
+        params = params or {'X-Emby-Token': self.api_key}
+        params.update({'X-Emby-Token': self.api_key})
+        url = rf'{self.host}/emby/{path}'
+        return self.req.post(
+            url,
+            json=_json,
+            params=params,
+        )
+
+    def get_genre_id(self, name):
+        res = self.get(f'Genres/{name}').json()['Id']
+        return res
+
+    def get_items(self, genre='', types='Movie,Series,Video', fields='CommunityRating,CriticRating', start_index=0,
+                  ids=None, limit=50,
+                  sort_by='DateCreated,SortName',
+                  recursive=True):
+        params = {
+            'HasTmdbId': True,
+            'SortBy': sort_by,
+            'SortOrder': 'Descending',
+            'IncludeItemTypes': types,
+            'Recursive': recursive,
+            'Fields': fields,
+            'StartIndex': start_index,
+            'Limit': limit,
+            'X-Emby-Token': self.api_key,
+        }
+
+        if genre:
+            params.update({'GenreIds': self.get_genre_id(genre)})
+        if ids:
+            params.update({'Ids': ids})
+
+        res = self.get('Items', params=params)
+        return res.json()
+
+    def yield_all_items(self, genre='', types='Movie,Series,Video', fields='CommunityRating,CriticRating',
+                        start_index=0, limit=200):
+        fist = self.get_items(genre=genre, types=types, fields=fields, start_index=start_index, limit=limit)
+        total = fist['TotalRecordCount']
+        yield from fist['Items']
+        for i in range(1, (total - start_index) // limit + 1):
+            _start_index = i * limit + start_index
+            yield from self.get_items(genre=genre, types=types, fields=fields, start_index=_start_index,
+                                      limit=limit)['Items']
+
+    def update_critic_rating(self, item_id, rating):
+        get_path = f'/Users/{self.user_id}/Items/{item_id}'
+        post_path = f'Items/{item_id}'
+        old = self.get(path=get_path, params={'Fields': 'ChannelMappingInfo'}).json()
+
+        useful_key = ['Name', 'OriginalTitle', 'Id', 'DateCreated', 'SortName', 'ForcedSortName', 'PremiereDate',
+                      'OfficialRating', 'Overview', 'Taglines', 'Genres', 'CommunityRating', 'RunTimeTicks',
+                      'ProductionYear', 'ProviderIds', 'People', 'Studios', 'TagItems', 'Status', 'DisplayOrder',
+                      'LockedFields', 'LockData']
+        _not_require_key = ['ServerId', 'Etag', 'CanDelete', 'CanDownload', 'PresentationUniqueKey', 'ExternalUrls',
+                            'Path', 'FileName', 'PlayAccess', 'RemoteTrailers', 'IsFolder', 'ParentId', 'Type',
+                            'GenreItems', 'LocalTrailerCount', 'UserData', 'RecursiveItemCount', 'ChildCount',
+                            'DisplayPreferencesId', 'AirDays', 'PrimaryImageAspectRatio', 'ImageTags',
+                            'BackdropImageTags']
+        # useful_key.append('ExternalUrls') # 无法更改
+        new = {k: v for k, v in old.items() if k in useful_key}
+        new.update({'CriticRating': rating})
+        self.post(path=post_path,
+                  _json=new)
+
+    def refresh(self, item_id):
+        self.post(f'Items/{item_id}/Refresh',
+                  _json={
+                      'Recursive': False,
+                      'MetadataRefreshMode': 'FullRefresh',
+                      'ReplaceAllMetadata': False,
+                  })
+
+
+class BangumiApi:
+    def __init__(self, http_proxy=None):
+        self.host = 'https://api.bgm.tv/v0/'
+        self.req = requests.Session()
+        self.req.headers.update({'Accept': 'application/json',
+                                 'Connection': 'keep-alive',
+                                 'User-Agent': 'kjtsune/embyBangumi'})
+        if http_proxy:
+            self.req.proxies = {'http': http_proxy, 'https': http_proxy}
+
+    def post(self, path, _json, params=None):
+        res = self.req.post(f'{self.host.rstrip("/")}/{path.lstrip("/")}',
+                            json=_json, params=params)
+        return res
+
+    def search(self, title, start_date, end_date, limit=5):
+        res = self.post('search/subjects',
+                        _json={"keyword": title,
+                               "filter": {"type": [2],
+                                          "air_date": [f'>={start_date}',
+                                                       f'<{end_date}'],
+                                          "nsfw": True}},
+                        params=dict(limit=limit))
+        return res.json()
+
+
+class BangumiApiEmbyVer(BangumiApi):
+    @staticmethod
+    def _emby_filter(bgm_data):
+        useful_key = ['date', 'id', 'name', 'name_cn', 'rank', 'score', ]
+        update_date = str(datetime.date.today())
+        if isinstance(bgm_data, list):
+            res = []
+            for data in bgm_data:
+                d = {k: v for k, v in data.items() if k in useful_key}
+                d['update_date'] = update_date
+                res.append(d)
+            return res
+        else:
+            d = {k: v for k, v in bgm_data.items() if k in useful_key}
+            d['update_date'] = update_date
+            return d
+
+    def emby_search(self, title, premiere_date: str, is_movie=False):
+        air_date = datetime.datetime.fromisoformat(premiere_date[:10])
+        start_date = air_date - datetime.timedelta(days=2)
+        day_after = 200 if is_movie else 2
+        end_date = air_date + datetime.timedelta(days=day_after)
+        bgm_data = self.search(title=title, start_date=start_date, end_date=end_date)
+        return self._emby_filter(bgm_data['data'])
+
+
+class JsonDataBase:
+    def __init__(self, name, prefix='', db_type='dict', workdir=None):
+        self.file_name = f'{prefix}_{name}.json' if prefix else f'{name}.json'
+        self.file_path = os.path.join(workdir, self.file_name) if workdir else self.file_name
+        self.db_type = db_type
+        self.data = self.load()
+
+    def load(self, encoding='utf-8'):
+        try:
+            with open(self.file_path, encoding=encoding) as f:
+                _json = json.load(f)
+        except (FileNotFoundError, ValueError):
+            print(f'{self.file_name} not exist, return {self.db_type}')
+            return dict(list=[], dict={})[self.db_type]
+        else:
+            return _json
+
+    def dump(self, obj, encoding='utf-8'):
+        with open(self.file_path, 'w', encoding=encoding) as f:
+            json.dump(obj, f, indent=2, ensure_ascii=False)
+
+    def save(self):
+        self.dump(self.data)
+
+
+class TmdbBgmDataBase(JsonDataBase):
+    def __getitem__(self, tmdb_id):
+        data = self.data.get(tmdb_id)
+        if not data:
+            return
+        air_date = datetime.date.fromisoformat(data['date'])
+        today = datetime.date.today()
+        if air_date + datetime.timedelta(days=90) > today:
+            expire_day = 3
+        elif air_date + datetime.timedelta(days=365) > today:
+            expire_day = 30
+        else:
+            expire_day = 120
+        update_date = datetime.date.fromisoformat(data['update_date'])
+        if update_date + datetime.timedelta(days=expire_day) < today:
+            return
+        return data
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def clean_not_trust_data(self, expire_days=7, min_trust=0.5):
+        expire_days = datetime.timedelta(days=expire_days)
+        today = datetime.date.today()
+        self.data = {_id: info for _id, info in self.data.items()
+                     if info['trust'] >= min_trust or
+                     datetime.date.fromisoformat(info['update_date']) + expire_days > today}
+        self.save()
+
+    def recount_trust_score(self):
+        res = {}
+        for _id, info in self.data.items():
+            trust = max(difflib.SequenceMatcher(None, info['name'], info['emby_ori']).quick_ratio(),
+                        difflib.SequenceMatcher(None, info['name_cn'], info['emby_name']).quick_ratio(),
+                        difflib.SequenceMatcher(None, info['name'], info['emby_name']).quick_ratio())
+            info['trust'] = trust
+            res[_id] = info
+        self.data = res
+        self.save()
+
+
+class NotResultDataBase(JsonDataBase):
+
+    def is_not_result(self, tmdb_id):
+        day_str = self.data.get(tmdb_id)
+        if not day_str:
+            return False
+        expire_date = datetime.date.fromisoformat(day_str) + datetime.timedelta(days=7)
+        if expire_date > datetime.date.today():
+            return True
+        else:
+            del (self.data[tmdb_id])
+            return False
+
+    def update_status(self, tmdb_id):
+        self.data[tmdb_id] = str(datetime.date.today())
+
+
+def update_critic_rating_by_bgm(emby: EmbyApi, bgm: BangumiApiEmbyVer, genre='动画', types='Movie,Series,Video',
+                                req_limit=50,
+                                item_limit=100, start_index=0, dry_run=True):
+    tmdb_db = TmdbBgmDataBase('tmdb_bgm')
+    tmdb_db.clean_not_trust_data()
+    # tmdb_db.clean_not_trust_data(expire_days=0, min_trust=0.5)
+    # tmdb_db.recount_trust_score()
+    # return
+    not_result_db = NotResultDataBase('tmdb_bgm_not_result')
+    tmdb_list = []
+    req_count = 0
+    item_count = 0
+
+    for item in emby.yield_all_items(genre=genre, types=types,
+                                     fields=','.join([
+                                         'PremiereDate',
+                                         'ProviderIds',
+                                         'CommunityRating',
+                                         'CriticRating',
+                                         'OriginalTitle',
+                                     ]),
+                                     start_index=start_index):
+
+        if req_count >= req_limit or item_count >= item_limit:
+            break
+
+        tmdb_id = item['ProviderIds']['Tmdb']
+        is_movie = False if item['Type'] == 'Series' else True
+        if tmdb_id in tmdb_list:
+            continue
+        else:
+            tmdb_list.append(tmdb_id)
+
+        # if is_movie:
+        #     continue
+        emby_name = item['Name']
+        emby_ori = item.get('OriginalTitle', '')
+        print(emby_name, end=' ')
+        item_count += 1
+
+        # emby.refresh(item['Id'])
+        # continue
+        re_split = re.compile(r'[／/]')
+        if re_split.search(emby_ori):
+            emby_ori = re_split.split(emby_ori)
+            for _t in emby_ori:
+                if any((bool(0x3040 <= ord(i) <= 0x30FF)) for i in _t):
+                    emby_ori = _t
+                    break
+            else:
+                emby_ori = emby_ori[0]
+        title = emby_ori or emby_name
+        premiere_date = item.get('PremiereDate')
+        if not premiere_date:
+            print('not PremiereDate, skip')
+            continue
+
+        if not_result_db.is_not_result(tmdb_id=tmdb_id):
+            print('\n^^^ not result ^^^\n')
+            continue
+
+        bgm_old = tmdb_db[tmdb_id]
+        bgm_data = bgm_old or bgm.emby_search(title=title, premiere_date=premiere_date)
+        req_count = req_count if bgm_old else req_count + 1
+        if emby_ori and not bgm_data:
+            bgm_data = bgm.emby_search(title=emby_name, premiere_date=premiere_date)
+            req_count += 1
+            print(f' | {emby_ori} :not result', end=' | ')
+            if not bgm_data:
+                print(f'{emby_name} :not result', end=' | ')
+                if is_movie:
+                    print(f'{emby_ori} :try without premiere_date', end=' | ')
+                    bgm_data = bgm.emby_search(title=emby_ori, premiere_date=premiere_date, is_movie=True)
+                    bgm_data or print(f'{emby_name} :try without premiere_date', end=' | ')
+                    req_count = req_count + 1 if bgm_data else req_count + 2
+                    bgm_data = bgm_data or bgm.emby_search(title=emby_name, premiere_date=premiere_date, is_movie=True)
+                if not bgm_data:
+                    not_result_db.update_status(tmdb_id=tmdb_id)
+                    print('\n^^^ not result ^^^\n')
+        if not bgm_data:
+            continue
+
+        bgm_data = bgm_data if bgm_old else bgm_data[0]
+        trust = bgm_data.get('trust') or max(difflib.SequenceMatcher(None, bgm_data['name'], emby_ori).quick_ratio(),
+                                             difflib.SequenceMatcher(None, bgm_data['name_cn'],
+                                                                     emby_name).quick_ratio(),
+                                             difflib.SequenceMatcher(None, bgm_data['name'], emby_name).quick_ratio())
+        if not bgm_old:
+            bgm_data['emby_ori'] = emby_ori
+            bgm_data['emby_name'] = emby_name
+            bgm_data['premiere_date'] = premiere_date
+            bgm_data['trust'] = trust
+            tmdb_db[tmdb_id] = bgm_data
+        if trust < 0.5:
+            print('\n^^^ trust < 0.5 ^^^\n')
+            continue
+        if not dry_run:
+            emby.update_critic_rating(item['Id'], bgm_data['score'] * 10)
+        print(f">>>> {bgm_data['name_cn']}[{bgm_data['name']}] {bgm_data['score']}")
+        if req_count % 50 == 0:
+            not_result_db.save()
+            tmdb_db.save()
+    if req_count > 0:
+        not_result_db.save()
+        tmdb_db.save()
+
+    print(f'\napi.bgm.tv requests count {req_count}')
+    print(f'total update count {item_count}')
+    res = emby.get_items(genre=genre, types=types)
+    print('total items count', res['TotalRecordCount'])
+    print(f'tmdb_bgm.json count {len(tmdb_db.data)}')
+
+
+def main():
+    conf = Configs()
+    http_proxy = conf.get('http_proxy', '') or None
+    my_emby = EmbyApi(
+        host=conf.get('host').split('/web/index')[0],
+        api_key=conf.get('api_key'),
+        user_id=conf.get('user_id'),
+        http_proxy=http_proxy
+    )
+    my_bgm = BangumiApiEmbyVer(http_proxy=http_proxy)
+    update_critic_rating_by_bgm(
+        emby=my_emby,
+        bgm=my_bgm,
+        genre=conf.get('genre'),
+        types=conf.get('types').strip(',').strip('，'),
+        req_limit=conf.get_int('req_limit', 50),
+        item_limit=conf.get_int('item_limit', 999),
+        start_index=conf.get_int('start_index', 0),
+        dry_run=conf.raw.getboolean('emby', 'dry_run', fallback=True)
+    )
+
+
+if __name__ == '__main__':
+    main()
